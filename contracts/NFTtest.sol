2afc8f69447de1f45c6c5b3c2691df270f6ab9b3
centie22 <vicentiapeace@gmail.com>
2022-08-19 21:12:12 -0400

    change

--- contracts/NFTtest.sol
+++ contracts/NFTtest.sol
@@ -1,71 +1,25 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Pausable.sol";
-import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
-import "@openzeppelin/contracts/utils/Context.sol";
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract VeeNFT is
-Context,
-AccessControlEnumerable,
-ERC721Enumerable,
-ERC721Burnable,
-ERC721Pausable
-{
-using Counters for Counters.Counter;
-bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
-bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
-Counters.Counter private _tokenIdTracker;
-string private _baseTokenURI;
+contract VEEsNFT is ERC721URIStorage{
+    using Counters for Counters.Counter;
 
-constructor(
-string memory name,
-string memory symbol,
-string memory baseTokenURI
-) ERC721(name, symbol) {
-_baseTokenURI = baseTokenURI;
-_setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-_setupRole(MINTER_ROLE, _msgSender());
-_setupRole(PAUSER_ROLE, _msgSender());
-}
-function _baseURI() internal view virtual override returns (string memory) {
-return _baseTokenURI;
-}
+    Counters.Counter private _myCounter;
+    uint256 MAX_SUPPLY = 4;
 
-function mint(address to) public virtual {
-require(hasRole(MINTER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have minter role to mint");
+    constructor() ERC721("VeeNFT", "OF") {}
 
-_mint(to, _tokenIdTracker.current());
-_tokenIdTracker.increment();
-}
+    function safeMint(address to, string memory uri) public{
 
-function pause() public virtual {
-require(hasRole(PAUSER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have pauser role to pause");
-_pause();
-}
+        uint256 tokenId = _myCounter.current();
+        require(tokenId <= MAX_SUPPLY, "Oops!, all NFTs have been minted!");
+        _myCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
 
-function unpause() public virtual {
-require(hasRole(PAUSER_ROLE, _msgSender()), "ERC721PresetMinterPauserAutoId: must have pauser role to unpause");
-_unpause();
-}
-function _beforeTokenTransfer(
-address from,
-address to,
-uint256 tokenId
-) internal virtual override(ERC721, ERC721Enumerable, ERC721Pausable) {
-super._beforeTokenTransfer(from, to, tokenId);
-}
 
-function supportsInterface(bytes4 interfaceId)
-public
-view
-virtual
-override(AccessControlEnumerable, ERC721, ERC721Enumerable)
-returns (bool)
-{
-return super.supportsInterface(interfaceId);
-}
 }
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,21 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  // const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  // const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
 
-  // const lockedAmount = ethers.utils.parseEther("1");
 
-  const VeeNFT = await ethers.getContractFactory("VeeNFT");
-  const veeNFT = await VeeNFT.deploy("VEE", "Vic", "ipfs://QmNgmtCG3ZsX65v7t4DbbHxpZ8CRD6xx3X2sDgoSff88AJ");
+  const VEEsNFT = await ethers.getContractFactory("VEEsNFT");
+  const veesNFT = await VEEsNFT.deploy();
 
-  await veeNFT.deployed();
+  await veesNFT.deployed();
 
-  console.log("VEE's NFT has been deployed to:  ", veeNFT.address );
+  console.log("NFT has been deployed to: ",  veesNFT.address);
 
-  const Mint = await veeNFT.mint("0x049C780d7fa94AA70194eFC88ee109781eaeE1C2")
+  const Mint = await veesNFT.safeMint("0x049C780d7fa94AA70194eFC88ee109781eaeE1C2", "ipfs://QmNgmtCG3ZsX65v7t4DbbHxpZ8CRD6xx3X2sDgoSff88AJ");
+  console.log("our mint", Mint)
 
-  console.log("the mint has been sent to:  ", Mint);
+  // This is my contract address 0x45748698CBb8840424908aa6B85242080D22fA28
+  // This is my url https://testnets.opensea.io/assets/rinkeby/0x45748698cbb8840424908aa6b85242080d22fa28/0
 }
-
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
@@ -27,7 +24,4 @@ main().catch((error) => {
   process.exitCode = 1;
 });
 
-
-//TRANSACTION HASH---- '0x3098e5e992c735ac1d1caaf726fd38db88ec23f8ea1d515acf69ede667d7e3d8'
-//CONTRACT ADDRESS --- "0xa55774A55094493ac407d2e58a32E5771D66e79e"
-//NFT LINK --- https://testnets.opensea.io/assets/rinkeby/0xa55774a55094493ac407d2e58a32e5771d66e79e/0/
+// CONTRACTADDRESS on Rinkeby = 0x340bB5bC82c7bcEaF942f893Bef7216490781775
